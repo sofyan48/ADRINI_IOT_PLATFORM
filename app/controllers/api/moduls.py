a7661e157a458f676706d8bcba0dbbf4c2ff373a
Sofyan Saputra <meongbego@gmail.com>
2019-02-22 14:09:29 +0700

    Add: Widget Correlation moduls

--- app/controllers/api/moduls.py
+++ app/controllers/api/moduls.py
@@ -13,14 +13,16 @@ class SendModuls(Resource):
     def get(self, id_channels):
         args = request.args
         report = []
+        data_widget = db.get_by_id("tb_widget", "id_channels",id_channels)
         for key in args :
+            for i in data_widget:
+                if i['nm_widget'] == key:
+                    data_insert = {
+                        "id_widget" : str(i['id_widget']),
+                        "value_field" : args[key]
+                    }
             message = []
-            data_insert = {
-                "id_channels" : id_channels,
-                "nm_field" : key,
-                "value_field" : args[key]
-            }
-            # print(data_insert)
+            print(data_insert)
             try:
                 result = db.insert(table="tb_moduls", data=data_insert)
             except Exception as e:
@@ -31,8 +33,8 @@ class SendModuls(Resource):
                 report.append(message)
             else:
                 message = {
-                    "data": data_insert,
-                    "id" : result
+                    "id" : result,
+                    key : data_insert
                 }
                 report.append(message)
         return response(200, data=report)
@@ -41,30 +43,13 @@ class SendModuls(Resource):
 class GetChannelsData(Resource):
     @apikey_required
     def get(self, id_channels):
-        obj_moduls = []
         limit= request.args['count']
         if limit == "" or limit == "0":
-            results = db.get_by_id(
-                        table="tb_moduls",
-                        field="id_channels",
-                        value=id_channels
-                    )
-
-            for i in results :
-                data = {
-                    "id_moduls": str(i['id_moduls']),
-                    "id_channels" : str(i['id_channels']),
-                    i['nm_field'] : i['value_field'],
-                    "created_at" : str(i['created_at']),
-                }
-                obj_moduls.append(data)
-            return response(200, data=obj_moduls)
-        else:
             result_data= list()
-            column = db.get_columns("tb_moduls")
+            column = db.get_columns("v_moduls")
             try:
                 result = list()
-                query = """select * from tb_moduls where id_channels="""+id_channels+""" order by created_at desc limit """+limit
+                query = """select * from v_moduls where id_channels="""+id_channels+""" order by created_at desc"""
                 print(query)
                 dbq.execute(query)
                 rows = dbq.fetchall()
@@ -81,7 +66,36 @@ class GetChannelsData(Resource):
                     data = {
                         "id_moduls": str(i['id_moduls']),
                         "id_channels" : str(i['id_channels']),
-                        i['nm_field'] : i['value_field'],
+                        "id_widget" : str(i['id_widget']),
+                        i['nm_widget'] : i['value_field'],
+                        "created_at" : str(i['created_at']),
+                    }
+                    result_data.append(data)
+                return response(200, data=result_data)
+        else:
+            result_data= list()
+            column = db.get_columns("v_moduls")
+            try:
+                result = list()
+                query = """select * from v_moduls where id_channels="""+id_channels+""" order by created_at desc limit """+limit
+                print(query)
+                dbq.execute(query)
+                rows = dbq.fetchall()
+                for row in rows:
+                    result.append(dict(zip(column, row)))
+            except Exception as e:
+                respons = {
+                    "status": False,
+                    "error": str(e)
+                }
+                return response(200, data=respons)
+            else:
+                for i in result :
+                    data = {
+                        "id_moduls": str(i['id_moduls']),
+                        "id_channels" : str(i['id_channels']),
+                        "id_widget" : str(i['id_widget']),
+                        i['nm_widget'] : i['value_field'],
                         "created_at" : str(i['created_at']),
                     }
                     result_data.append(data)
@@ -93,15 +107,14 @@ class GetChannelDataByField(Resource):
         field = None
         for i in args:
             if field is None:
-                field = "nm_field='"+args[i]+"'"
+                field = "nm_widget='"+args[i]+"'"
             else:
-                field = field+" or nm_field='"+args[i]+"'"
+                field = field+" or nm_widget='"+args[i]+"'"
         result_data= list()
-        column = db.get_columns("tb_moduls")
+        column = db.get_columns("v_moduls")
         try:
             result = list()
-            query = """select * from tb_moduls where id_channels="""+id_channels+""" and """+field+""" order by created_at desc limit """+page
-            print(query)
+            query = """select * from v_moduls where id_channels="""+id_channels+""" and """+field+""" order by created_at desc limit """+page
             dbq.execute(query)
             rows = dbq.fetchall()
             for row in rows:
@@ -113,12 +126,20 @@ class GetChannelDataByField(Resource):
             }
             return response(200, data=respons)
         else:
+            arr_field = dict(args)
             for i in result :
+                field = None
+                for a in arr_field:
+                    if arr_field[a] == i['nm_widget']:
+                        field = a
                 data = {
-                    "id_moduls": str(i['id_moduls']),
-                    "id_channels" : str(i['id_channels']),
-                    i['nm_field'] : i['value_field'],
-                    "created_at" : str(i['created_at']),
+                    field : {
+                        "id_moduls": str(i['id_moduls']),
+                        "id_channels" : str(i['id_channels']),
+                        "id_widget" : str(i['id_widget']),
+                        i['nm_widget'] : i['value_field'],
+                        "created_at" : str(i['created_at']),
+                    }
                 }
                 result_data.append(data)
             return response(200, data=result_data)
