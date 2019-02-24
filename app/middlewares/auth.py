7f4d7ef204787d7759d2fd872946ad808d360c53
meongbego <meongbego@gmail.com>
2019-02-24 17:32:14 +0700

    Change: int to str return id

--- app/middlewares/auth.py
+++ app/middlewares/auth.py
@@ -21,7 +21,7 @@ def my_expired_token_callback():
 
 @jwt.user_identity_loader
 def user_identity_lookup(user):
-    return user['id_userdata']
+    return str(user['id_userdata'])
 
 
 def user_loader(fn):
